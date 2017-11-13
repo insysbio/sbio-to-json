fc33d50d69055e0e83e7b8a3b5d52cc6da4e627f
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2017-11-13 18:34:00 +0300

    add license notice

--- getGet.m
+++ getGet.m
@@ -1,3 +1,19 @@
+%{
+   Copyright 2017 InSysBio, LLC
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+%}
+
 function out = getGet(x)
     if length(x)>1 && not(strcmp(class(x),'char'))
         %disp(x.Type);
@@ -20,7 +36,3 @@ function out = getGet(x)
     	out = x;
     end
 end
-
-%class()
-%get() % get all properties
-%isa(x, 'double')
